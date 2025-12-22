3870075a9d73966770604440283b62b77cf8402e
Ashmit Dutta <ashmitdutta@gmail.com>
2025-12-22 23:59:27 +0530

    implements database setup

---
+++ database/mongoose.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  } | undefined;
+}
+
+let cached = global.mongooseCache;
+if (!cached) cached = global.mongooseCache = { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error("Please set MONGODB_URI in .env.local");
+  }
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    mongoose.set("strictQuery", true);
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      bufferCommands: false,
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+    console.log(`✅ MongoDB connected:${process.env.NODE_ENV} phase - ${MONGODB_URI}`);
+    return cached.conn;
+  } catch (err) {
+    cached.promise = null;
+    console.error("❌ MongoDB connection error", err);
+    throw err;
+  }
+};
